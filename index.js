9e444a3f1e3287a9667acbe9eab61b186482a870
Abel Adane <contacttoabel@gmail.com>
2022-12-06 16:20:02 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const user = {
+    firstName: 'John',
+    lastName: 'Doe'
+}
+
+const handler = {
+    get(target, property) {
+        return property === 'fullName' ?
+            `${target.firstName} ${target.lastName}` :
+            target[property];
+    }
+};
+
+const proxyUser = new Proxy(user, handler);
+
+console.log(proxyUser.fullName);
